e9ff054a6cea5400d69453d1703aedd8db1e99b8
zgyr <0112d759@gmail.com>
2021-01-02 19:59:59 +0600

    updated installer.lua

---
+++ installer.lua
@@ -0,0 +1,3 @@
+os.execute('mkdir /usr/bin')
+os.execute('wget https://raw.githubusercontent.com/zgyr/bpacker/master/bpacker.lua /usr/bin/bpacker.lua')
+os.execute('wget https://raw.githubusercontent.com/stravant/lua-minify/master/minify.lua /usr/bin/minify.lua')
