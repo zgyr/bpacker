ed587b4d5d755b71901a62fb911b3237feb09dcd
zgyr <60771101+zgyr@users.noreply.github.com>
2021-01-08 15:05:01 +0600

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -1,5 +1,6 @@
-os.execute('mkdir /usr/bin')
-os.execute('mkdir /usr/lib')
-os.execute('wget https://raw.githubusercontent.com/BrightYC/Other/master/lzss.lua /usr/lib/lzss.lua')
-os.execute('wget https://raw.githubusercontent.com/zgyr/bpacker/master/bpacker.lua /usr/bin/bpacker.lua')
-os.execute('wget https://raw.githubusercontent.com/stravant/lua-minify/master/minify.lua /usr/bin/minify.lua')
+local fs = require('filesystem')
+if not fs.exists('/usr/bin') then fs.makeDirectory('/usr/bin') end
+if not fs.exists('/usr/lib') then fs.makeDirectory('/usr/lib') end
+os.execute('wget -f https://raw.githubusercontent.com/BrightYC/Other/master/lzss.lua /usr/lib/lzss.lua')
+os.execute('wget -f https://raw.githubusercontent.com/zgyr/bpacker/master/bpacker.lua /usr/bin/bpacker.lua')
+os.execute('wget -f https://raw.githubusercontent.com/stravant/lua-minify/master/minify.lua /usr/bin/minify.lua')
